2d56f5e0cdd2cea5517419a32f450c6b0c26b7a1
Shamshad Akther <shamshad.cse90@gmail.com>
2019-11-19 17:36:41 +0200

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -4,6 +4,7 @@ import data from '../services/data.json';
 export default axios.create({
   baseURL: data.constants.site_url,
   responseType: "json",
+  // used below code for only free hosting url
   withCredentials: true,
   headers: {
           Cookie:'__test=1b1cf0780f2f9f754ffe07e34d67a235;expires=Thu, 31-Dec-37 23:55:55 GMT; path=/'
