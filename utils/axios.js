73ac1009749dd1ed0b4a6fd9fd3eaaebd418b44e
Shamshad Akther <shamshad.cse90@gmail.com>
2019-11-19 07:54:01 +0200

    Add files via upload

--- utils/axios.js
+++ utils/axios.js
@@ -3,5 +3,9 @@ import data from '../services/data.json';
 
 export default axios.create({
   baseURL: data.constants.site_url,
-  responseType: "json"
+  responseType: "json"  
+  withCredentials: true,
+  headers: {
+          Cookie:'__test=1b1cf0780f2f9f754ffe07e34d67a235;expires=Thu, 31-Dec-37 23:55:55 GMT; path=/'
+        },
 });
