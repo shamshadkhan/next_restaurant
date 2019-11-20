c83ebc0bac5340c2d4bca6572a3f79aa9477a8bf
Shamshad Akther <shamshad.cse90@gmail.com>
2019-11-20 09:55:09 +0200

    Update axios.js

--- utils/axios.js
+++ utils/axios.js
@@ -5,8 +5,8 @@ export default axios.create({
   baseURL: data.constants.site_url,
   responseType: "json",
   // used below code for only free hosting url
-  withCredentials: true,
-  headers: {
-          Cookie:'__test=1b1cf0780f2f9f754ffe07e34d67a235;expires=Thu, 31-Dec-37 23:55:55 GMT; path=/'
-        },
+//   withCredentials: true,
+//   headers: {
+//           Cookie:'__test=1b1cf0780f2f9f754ffe07e34d67a235;expires=Thu, 31-Dec-37 23:55:55 GMT; path=/'
+//         },
 });
